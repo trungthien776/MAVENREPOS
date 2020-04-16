16f20591c88e6e768954e8c40b067d5a14d8945b
thien Truong <you@example.com>
2020-04-16 15:27:03 +0700

    add interface

---
+++ OTHER-WEB/src/main/java/com/clv/wpm/esb/working/DspringProfile.java
@@ -0,0 +1,12 @@
+package com.clv.wpm.esb.working;
+
+import java.io.Serializable;
+
+public class DspringProfile implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
