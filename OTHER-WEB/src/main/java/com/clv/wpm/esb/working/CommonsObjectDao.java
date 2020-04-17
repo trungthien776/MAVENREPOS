d38ba86ae583748954414d45c6124df4631ddb1f
thien Truong <you@example.com>
2020-04-17 09:19:41 +0700

    #3746

--- OTHER-WEB/src/main/java/com/clv/wpm/esb/working/CommonsObjectDao.java
+++ OTHER-WEB/src/main/java/com/clv/wpm/esb/working/CommonsObjectDao.java
@@ -5,4 +5,5 @@ import java.sql.SQLException;
 public interface CommonsObjectDao extends Cloneable {
 		public int getCntrPoint(String cntrNo);
 		public void getSeqNo(String cntrNo) throws SQLException;
+		public String getVessel(String cntrNo) throws SQLException;
 }
